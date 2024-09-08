2bdd727c7e7c6482ca0ede658f753d05d96120f7
Çağrı Dai <dai.cagri@gmail.com>
2024-09-08 16:54:15 +0300

    batman 2

--- src/textnode.py
+++ src/textnode.py
@@ -39,7 +39,7 @@ def text_node_to_html_node(text_node):
         return LeafNode("code", text_node.text)
     
     if text_node.text_type == text_type_link:
-        return LeafNode("a", text_node.text, {"url": text_node.url})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     
     if text_node.text_type == text_type_image:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
